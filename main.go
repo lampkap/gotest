527fd9c3fcb77bd1ac8a6e2e79073cbd9d7b2150
diemleyssen <diem@novation.be>
2022-04-27 10:34:02 +0200

    added basic auth

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/subtle"
 	"log"
 	"net/http"
 	"os"
@@ -26,9 +27,33 @@ func main() {
 
 func handleTransactions(r *mux.Router) {
 	s := r.PathPrefix("/api/transaction").Subrouter()
+	s.Use(AuthMiddleware)
 
 	s.HandleFunc("/all", api.GetTransactions)
 	s.HandleFunc("/add", api.CreateTransaction).Methods("POST")
 	s.HandleFunc("/update/{id}", api.UpdateTransaction).Methods("POST")
 	s.HandleFunc("/delete/{id}", api.DeleteTransaction).Methods("DELETE")
+}
+
+// Handle Basic Auth for every route this middleware is added to.
+func AuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if BasicAuth(w, r) {
+			next.ServeHTTP(w, r)
+		}
+	})
+}
+
+func BasicAuth(w http.ResponseWriter, r *http.Request) bool {
+	username := os.Getenv("HTTP_USERNAME")
+	password := os.Getenv("HTTP_PASSWORD")
+	user, pass, ok := r.BasicAuth()
+
+	if !ok || subtle.ConstantTimeCompare([]byte(user), []byte(username)) != 1 || subtle.ConstantTimeCompare([]byte(pass), []byte(password)) != 1 {
+		w.WriteHeader(401)
+		w.Write([]byte("Unauthorised.\n"))
+		return false
+	}
+
+	return true
 }
